bb4f2c4bd492a2d1093c2a51ebe84a4ecc082b1f
Monasef <monasef77@gmail.com>
2023-09-05 20:02:52 +0100

    implement all functionalite without redux

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+const El = document.getElementById('root');
+const root = ReactDOM.createRoot(El);
+
+root.render(<App />);
