6224c2767c901bba796d0b293ad1ffe6879501a9
Monasef <monasef77@gmail.com>
2023-09-06 17:18:42 +0100

    implementing account and customer slices

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { StrictMode } from 'react';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 const El = document.getElementById('root');
 const root = ReactDOM.createRoot(El);
 
-root.render(<App />);
+root.render(
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </StrictMode>
+);
